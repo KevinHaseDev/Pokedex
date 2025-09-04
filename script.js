63bbbc53a32330bcaf5cf58f45ca5ae24cfa6942
Kevin Hase <hasekevin21@googlemail.com>
2025-09-04 10:12:19 +0200

    write pseudocode

---
+++ script.js
@@ -0,0 +1,7 @@
+let Pokedex = [];
+
+//wir wollen die daten fetchen von der pokeapi
+//es sollen erst die erste Gen geladen werden
+//mit jedem click auf load more kommt die nächste Gen
+//wir wollen uns die sprits den namen und den Typ ausgeben lassen
+//es soll ein dialog geben, mit mehr stats und der evochain
